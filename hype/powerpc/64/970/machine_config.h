ca455419db7c33f3725f633e69d9b32592640b57
jimix <devnull@localhost>
2005-02-13 17:39:34 +0000

    fixup the ID strings and comments

--- hype/powerpc/64/970/machine_config.h
+++ hype/powerpc/64/970/machine_config.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (C) 2005 Jimi Xenidis <jimix@watson.ibm.com>, IBM Corporation
  *
@@ -15,7 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
+ *
+ * $Id$
  *
  */
 
