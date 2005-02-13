8596e22da6878b6d36cc46f017589991f5173ea1
jimix <devnull@localhost>
2005-02-13 17:39:25 +0000

    fixup the ID strings and comments

--- hype/eic.h
+++ hype/eic.h
@@ -1,5 +1,3 @@
-#ifndef _EIC_H
-#define _EIC_H
 /*
  * Copyright (C) 2005 Jimi Xenidis <jimix@watson.ibm.com>, IBM Corporation
  *
@@ -16,9 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
  *
+ * $Id$
  */
+#ifndef _EIC_H
+#define _EIC_H
+
 #include <config.h>
 #include <types.h>
 #include <cpu_thread.h>
