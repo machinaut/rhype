b10e900b75adb8518e41d2615245812073ebcedc
jimix <devnull@localhost>
2005-02-13 17:39:28 +0000

    fixup the ID strings and comments

--- hype/rcu.h
+++ hype/rcu.h
@@ -14,9 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
- * Declarations for hypervisor hcall()-implementation functions.
  *
+ * $Id$
  */
 #ifndef _RCU_H
 #define _RCU_H
