f121c1a74d2d8a88f459679954b183470c70c6a4
jimix <devnull@localhost>
2005-02-13 17:39:35 +0000

    fixup the ID strings and comments

--- hype/x86/h_set_mbox.c
+++ hype/x86/h_set_mbox.c
@@ -14,8 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
  *
+ * $Id$
+ */
+/*
  * Each partition has a mailbox, into which both the
  * OS and the hv can write. This is aimed at eliminating
  * hcalls to get/set information.
