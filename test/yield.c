1bf6213e7c712c82e6aee6b7a3f9882b7d7dad7f
jimix <devnull@localhost>
2005-02-13 17:39:48 +0000

    fixup the ID strings and comments

--- test/yield.c
+++ test/yield.c
@@ -14,9 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
- * "Smoke" test of an OS yielding when there is no other OS to yield to.
  *
+ * $Id$
+ */
+
+/*
+ * "Smoke" test of an OS yielding when there is no other OS to yield to.
  */
 #include <test.h>
 #include <lpar.h>
