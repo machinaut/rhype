036419c79bd9b0a86c2056535192736b34e504e5
jimix <devnull@localhost>
2005-02-13 17:39:37 +0000

    fixup the ID strings and comments

--- hype/x86/pgcache.h
+++ hype/x86/pgcache.h
@@ -14,8 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
  *
+ * $Id$
  */
 #ifndef __HYPE_X86_PGCACHE_H__
 #define __HYPE_X86_PGCACHE_H__
