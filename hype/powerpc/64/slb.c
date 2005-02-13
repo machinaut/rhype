9e48795404adb1b6cd9b4ffcacb1ffef8876cc4f
jimix <devnull@localhost>
2005-02-13 17:39:33 +0000

    fixup the ID strings and comments

--- hype/powerpc/64/slb.c
+++ hype/powerpc/64/slb.c
@@ -14,7 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
+ *
+ * $Id$
+ */
+/*
  * Allocate and deallocate OS-related data structures.
  * This is globally locked -- if you are creating and destroying
  * OSes quickly enough to stress this lock, you have many other
