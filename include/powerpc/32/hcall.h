ce0b6c416f8dc09c47d3987bd7e15a3f74979220
jimix <devnull@localhost>
2005-02-13 17:39:39 +0000

    fixup the ID strings and comments

--- include/powerpc/32/hcall.h
+++ include/powerpc/32/hcall.h
@@ -14,12 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
  *
- * Details of the Broadband Processor Architecture as described in the:
- *  Broadband Processor Baseline v. 1.0
- *
+ * $Id$
  */
+
 #ifndef _PPC32_HCALL_H
 #define _PPC32_HCALL_H
 
