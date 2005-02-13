872c4bc3a864b2714e6d5171ea22eefe7722f500
jimix <devnull@localhost>
2005-02-13 17:39:48 +0000

    fixup the ID strings and comments

--- test/powerpc/64/set_asr.c
+++ test/powerpc/64/set_asr.c
@@ -14,10 +14,14 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- $Id$
- * "Smoke" test of H_SET_ASR hcalls.
  *
+ * $Id$
  */
+
+/*
+ * "Smoke" test of H_SET_ASR hcalls.
+ */
+
 #include <test.h>
 
 struct partition_info pinfo[2] = {{
