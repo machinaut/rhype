ad1583d6d0fda0f0085515ffc87895db77111a69
mostrows <devnull@localhost>
2005-02-16 17:37:56 +0000

    Split mmu.h files into mmu.h and mmu_defs.h.
    Keep mmu_defs.h clean, so as to allow it to be included in arbitrary
    code.  mmu_defs are to simply provide definitions of things such as
    PGSIZE.  mmu.h can contain function definitions and code.
    
    (Without this only rhype code can use mmu.h, as there are function
    definitions which conflict in other contexts --- definition of sync)

--- include/powerpc/64/mmu.h
+++ include/powerpc/64/mmu.h
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
@@ -27,28 +27,8 @@
 #include <types.h>
 #include <partition.h>
 #include <util.h>
+#include <mmu_defs.h>
 
-#define BOOT_STACK_SIZE (20 * PGSIZE)
-
-#ifdef CPU_4xx
-#define LOG_PGSIZE 10		/* XXX what? 440 page sizes are variable... */
-#elif HAS_64BIT
-#define LOG_PGSIZE 12
-#endif
-#define PGSIZE		((1ULL) << LOG_PGSIZE)
-#define PGMASK		(~(PGSIZE-1))
-#define PGALIGN(a)	ALIGN_UP(a, PGSIZE)
-/* returns number of pages for a size s */
-#define PGNUMS(s)	(PGALIGN(s) >> LOG_PGSIZE)
-
-#define LOG_LARGE_PGSIZE 24
-#define LARGE_PGSIZE ((1ULL) << LOG_LARGE_PGSIZE)
-
-/* pte bits */
-#define PAGE_WIMG_G	0x008UL	/* G: Guarded */
-#define PAGE_WIMG_M	0x010UL	/* M: enforce memory coherence */
-#define PAGE_WIMG_I	0x020UL	/* I: cache inhibit */
-#define PAGE_WIMG_W	0x040UL	/* W: cache write-through */
 
 enum { lpg0, lpg1, lpg2 };
 
